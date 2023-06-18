f97e250a3a36c0a8f06afa264efb988c3576228e
John Henry Galino <github.stolze@simplelogin.com>
2023-06-18 12:31:20 +0800

    Additional config

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Update the package manager
+sudo pacman -Syu
+
+# Install needed packages
+sudo pacman -S zsh kitty bluez bluez-utils blueman volumeicon ly nnn
+
+######### Set zsh as default shell ############
+
+# Change the default shell for the current user
+chsh -s $(which zsh)
+
+# Copy the default Zsh configuration
+cp zshrc /home/$(whoami)/.zshrc
+
+######### Set up terminal ############
+# Create a temporary directory
+temp_dir=$(mktemp -d)
+
+# Download the JetBrainsMono Nerd Font zip archive
+curl -Lo "$temp_dir/JetBrainsMono.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+
+# Extract the font files from the zip archive
+unzip "$temp_dir/JetBrainsMono.zip" -d "$temp_dir"
+
+# Install the font
+mkdir -p ~/.local/share/fonts
+cp "$temp_dir/"*.ttf ~/.local/share/fonts/
+
+# Update the font cache
+fc-cache -f -v
+
+# Clean up the temporary directory
+rm -rf "$temp_dir"
+
+######### Set-up services ##########
+sudo systemctl enable bluetooth.service 
+sudo systemctl enable ly.service
+
+######### place configs in right places #######
+cp i3.config /home/$(whoami)/.config/i3/config
+cp kitty.config /home/$(whoami)/.config/kitty/kitty.conf
+cp black-wallpaper.png /home/$(whoami)/Backgrounds/black-wallpaper.png
---
+++ zshrc
@@ -0,0 +1,29 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/jhgalino/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+# Add hx as default editor
+export EDITOR=/usr/bin/hx
+export TERMINAL=kitty
+export BROWSER=firefox
+
+# Add scripts path
+export PATH=$PATH:~/AB_Scripts
+
+alias ls='ls --color=auto'
+
+# Package sizes
+alias pkg_size="expac -H M '%m\t%n' | sort -h"
+
+eval "$(starship init zsh)"
+export GPG_TTY=$(tty)
+
